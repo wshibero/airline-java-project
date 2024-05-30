e2f61d2c34d60bcedb6fd7999d11f410b8457165
wakhu shibero <wshibero@gmail.com>
2024-05-30 07:58:57 +0000

    package users

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,13 @@
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // import org.junit.jupiter.api.Test;
+import Users.Users;
 
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    Users user = new Users("John", "Doe", "johndoe@example.com", "password");
+    System.out.println(user);
   }
 
   // @Test
--- src/main/java/Users/Users.java
+++ src/main/java/Users/Users.java
@@ -1,6 +1,24 @@
+package Users;
+
 public class Users{
   private String fname;
   private String lname;
   private String email;
   private String password;
+
+  public Users(String fname, String lname, String email, String password){
+    this.fname = fname;
+    this.lname = lname;
+    this.email = email;
+    this.password = password;
+  }
+  public String toString(){
+    return 
+      "First Name: " + this.fname + 
+      "\nLast Name: " + this.lname + 
+      "\nEmail: " + this.email;
+  }
+
+  public class UserFactory{
+  }
 }
