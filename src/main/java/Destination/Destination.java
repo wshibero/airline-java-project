d0d09eb2543d04852b9885dc43bb0aff5420050f
wakhu shibero <wshibero@gmail.com>
2024-05-30 09:25:56 +0000

    added destination

---
+++ src/main/java/Destination/Destination.java
@@ -0,0 +1,11 @@
+package Destination;
+
+public class Destination{
+  private String destTown;
+  private String destCountry;
+  private String destContinent;
+  private String originTown;
+  private String originCountry;
+  private String originContinent;
+  private int cost;
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@
 
 // import org.junit.jupiter.api.Test;
 import Users.Users;
+import Destination.Destination;
 
 public class Main {
   public static void main(String[] args) {
